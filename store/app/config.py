37591de8d6bdb606777420768aa2e1f598677e42
Godriele Dela Cruz <godriele11@yahoo.com>
2024-11-03 15:06:45 -0800

    Initialized Flask Application

---
+++ store/app/config.py
@@ -0,0 +1,16 @@
+# Database URI Secret Keys
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.secret_key = "Sapatoes"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SQLALCHEMY_TRACK_MODIFCATIONS"] = False
+
+db = SQLAlchemy(app)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
